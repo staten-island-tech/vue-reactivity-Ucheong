cc52e6f6a309cf9f01281c4289a2bebb7ddb8803
Ucheong <ucheongsou@gmail.com>
2023-03-24 10:16:03 -0400

    vue

---
+++ vue/src/components/icons/filter.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { ref } from "vue";
+
+const newTodo = ref("");
+const todos = ref([]);
+
+function addTodo() {
+  todos.value.push({ text: newTodo.value });
+  newTodo.value = "";
+}
+
+function removeTodo(todo) {
+  todos.value = todos.value.filter((t) => t !== todo);
+}
+</script>
+
+<template>
+  <form @submit.prevent="addTodo">
+    <input v-model="newTodo" />
+    <button>Add Todo</button>
+  </form>
+  <ul>
+    <li v-for="todo in todos">
+      {{ todo.text }}
+      <button @click="removeTodo(todo)">X</button>
+    </li>
+  </ul>
+</template>
---
+++ vue/src/components/icons/monkeyArray.vue
@@ -0,0 +1,17 @@
+<template>
+  <div v-for="monkey in monkeys" :key="monkey">{{ monkey }}</div>
+</template>
+
+<script>
+export default {
+  name: "Monkeys",
+  components: {},
+  data() {
+    return {
+      monkeys: ["../imgs.Monkey1.jpg", ""],
+    };
+  },
+};
+</script>
+
+<style></style>
