787e7b0d5b83795e27a0fc7ec0175b883a0f8558
Ucheong <ucheongsou@gmail.com>
2023-04-02 23:06:43 -0400

    vue

---
+++ monkeyvue/src/components/monkeyArray.vue
@@ -0,0 +1,17 @@
+<template>
+  <div v-for="monkey in monkeys" :key="monkey">{{ monkey }}</div>
+</template>
+
+<script>
+export default {
+  name: "Monkeys",
+  components: {},
+  data() {
+    return {
+      props: [],
+    };
+  },
+};
+</script>
+
+<style></style>
